c26338612e8d67e20c8b71e4063ed9b1f65844e4
Pranjal Gupta <guptapranjal3105@gmail.com>
2019-07-22 00:47:59 +0530

    Configuration file and setting up connection

--- index.js
+++ index.js
@@ -13,15 +13,25 @@ app.get('/', function(req, res){
 // Grabbing up all of the Routes defined in the file
 const interviewbasedOperationsRoute = require('./Routes/InterviewCalls');
 
-// Connect to MongoDB
 
-// mongoose.connect('mongodb://localhost/InterviewsDB');
-mongoose.connect(`mongodb://${config.User}:${encodeURIComponent(config.Password)}@ds253537.mlab.com:53537/heroku_l7rc1lfr`);
+// Connect to MongoDB
+if(config.Environment == "Test")
+{
+var dbconnectionURL  = `localhost`;
+mongoose.connect(`mongodb://${dbconnectionURL}/${config.Database}`);
+mongoose.Promise = global.Promise;
+console.log("Connection Established For Test Env");
+}
+else
+{
+var dbconnectionURL  = `${config.User}:${encodeURIComponent(config.Password)}@${config.Host}:${config.Post}`;
+mongoose.connect(`mongodb://${dbconnectionURL}/${config.Database}`);
 // Had Problem with using @ in the Password of MongoDB solved using Answer Provided by vanduc1102
 // at : https://stackoverflow.com/questions/7486623/mongodb-password-with-in-it
 
 mongoose.Promise = global.Promise;
-
+console.log("Connection Established For Dev Env");
+}
 
 // Role : It is a Middle-ware, takes the Body as send by the Caller and attaches it to the Request's Body for further usage
 app.use(bodyparser.json());
